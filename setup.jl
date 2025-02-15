9d45b2e1998edbb0bd22bf6ce79ea2dceeb4752e
Hongkee Yoon <bluehope@gmail.com>
2025-02-16 00:11:21 +0900

    Works for julia 1.11

--- setup.jl
+++ setup.jl
@@ -1,39 +1,42 @@
 using Pkg
 
-PWD = ENV["PWD"]
-PROJECT_PATH_KAIEDJ = PWD*"/envs/KEDJ"
-ENV["PROJECT_PATH_KAIEDJ"] = PROJECT_PATH_KAIEDJ
+# PWD = ENV["PWD"]
+# PROJECT_PATH_KAIEDJ = PWD*"/envs/KEDJ"
+# ENV["PROJECT_PATH_KAIEDJ"] = PROJECT_PATH_KAIEDJ
 
-Pkg.activate(PROJECT_PATH_KAIEDJ)
+# Pkg.activate(PROJECT_PATH_KAIEDJ)
 
-ModuleNames = [ 
-                "Formatting",
-                "LinearAlgebra",
-                "SparseArrays",
-                "DelimitedFiles",
-                "FastGaussQuadrature",
-                "BenchmarkTools",
-                "Optimization",
-                "OptimizationOptimJL",
-                "Optim",
-                "Arpack",
-                "ThreadedSparseCSR",
-                "SparseMatricesCSR",
-                "Plots",
-                "TickTock",
-                "TimerOutputs",
-                "FFTW",
-                "JSON",
-                "Dierckx",
-                "ImageFiltering",
-                "HDF5",
-                "DFTforge"
-                ]
-ModuleNames = [ ]
-nmod = length(ModuleNames)
-for (imod, modname) in Iterators.enumerate(ModuleNames )
-    println( "[$(imod)/$(nmod)] $(modname) ..." )
-    Pkg.add(modname)
-end
+# ModuleNames = [ 
+#                 "Formatting",
+#                 "LinearAlgebra",
+#                 "SparseArrays",
+#                 "DelimitedFiles",
+#                 "FastGaussQuadrature",
+#                 "BenchmarkTools",
+#                 "Optimization",
+#                 "OptimizationOptimJL",
+#                 "Optim",
+#                 "Arpack",
+#                 # "ThreadedSparseCSR",
+#                 "SparseMatricesCSR",
+#                 "Plots",
+#                 "TickTock",
+#                 "TimerOutputs",
+#                 "FFTW",
+#                 "JSON",
+#                 "Dierckx",
+#                 "ImageFiltering",
+#                 "HDF5",
+#                 "DFTforge"
+#                 ]
+# ModuleNames = [ ]
+# nmod = length(ModuleNames)
+# for (imod, modname) in Iterators.enumerate(ModuleNames )
+#     println( "[$(imod)/$(nmod)] $(modname) ..." )
+#     Pkg.add(modname)
+# end
 
+
+Pkg.add(url="https://github.com/KAIST-ELST/ThreadedSparseCSR.jl")
+Pkg.add(path=".")
 include("scripts/patch_hotfix.jl")
